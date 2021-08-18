d72d0b25b29420fe7609ccfb691ae5c6dca33c09
David Salonga <fdc.salonga@gmail.com>
2021-08-18 18:01:15 +0800

    [React-ToDoList][Francis]  chore: added axios and bootstraps css

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {Provider} from "react-redux";
 import store from "./store/store";
 import "bootstrap/dist/css/bootstrap.css";
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
